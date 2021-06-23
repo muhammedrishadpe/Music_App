48ad52e3e21d04f042f601d97e49c3d6f17baf07
muhammedrishadpe <rishadpe@gmail.com>
2021-06-23 14:22:59 +0530

    Updated All

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 window.addEventListener('load', () => {
     const sounds = document.querySelectorAll('.sound');
     const pads = document.querySelectorAll('.pads div');
+    const visual = document.querySelector('.visual');
+    const colors = [
+        "#60d394",
+        "#d36060",
+        "#c060d3",
+        "#d3d160",
+        "#6860d3",
+        "#3e6b98"
+    ];
 
     //Lets get going with the sound here
     pads.forEach((pad, index) =>{
         pad.addEventListener('click', function() {
+            sounds[index].currentTime = 0;
            sounds[index].play();
+
+           createBubbles(index);
         });
     });
+
+    //Create a funcation that make bubbles
+    const createBubbles = (index) => {
+        const bubble = document.createElement("div");
+        visual.appendChild(bubble);
+        bubble.style.backgroundColor = colors[index];
+        bubble.style.animation = "jumb 1s ease";
+        bubble.addEventListener('animationend', function(){
+            visual.removeChild(this);
+        });
+    }
 });
+    
